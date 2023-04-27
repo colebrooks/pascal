b06477eff02c8817c4e543bfb77a53cc59630bf8
colebrooks <colespencerbrooks@gmail.com>
2023-04-27 11:28:53 -0600

    Implement reading of words in tokenizer

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,19 +1,40 @@
-#include<string.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "tokenizer.h"
 
 token tokens[255];
+int num_tokens = 0;
+
+void read_word(FILE *fp, char first_letter) {
+    char word[255] = "";
+    word[0] = first_letter;
+
+    int len = 1;
+    char c;
+    while(isalpha((c = fgetc(fp)))) {
+        word[len++] = c;
+    }
+    printf("Word: %s\n", word);
+}
+
+void read_tokens(FILE *fp) {
+    int current_token = 0;
+    char c;
+    while ((c = fgetc(fp)) != EOF) {
+        if(isalpha(c)) {
+            read_word(fp, c);
+        }
+    }
+    if (feof(fp)) {
+        return;
+    } else {
+        printf("Error reading tokens from file!\n");
+        return;
+    }
+}
 
 void tokenize(FILE *fp) {
-    char buff[255];
-    fgets(buff, 255, fp);
-
-    int current_token = 0;
-    while (!feof(fp)) {
-        fscanf(fp, "%s", buff);
-        token token;
-        strcpy(token.value, buff);
-        tokens[current_token++] = token;
-        printf("Token value: %s\n", token.value);
-    }
+    read_tokens(fp);
 } 
+

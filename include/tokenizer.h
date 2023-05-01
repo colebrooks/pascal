e6b460322f738ab7f64b344e1e3b7eacbe115b63
colebrooks <colespencerbrooks@gmail.com>
2023-05-01 15:17:10 -0600

    Move token types to tokenizer.h

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -1,11 +1,23 @@
+#ifndef TOKENIZER_H_
+#define TOKENIZER_H_
+
 #include <stdio.h>
 
-#include "token_types.h"
+enum token_type {
+    IDENTIFIER,
+    KEYWORD,
+    SEPARATOR,
+    OPERATOR,
+    LITERAL,
+    COMMENT
+};
 
 typedef struct token_t {
     enum token_type type;
-    char value[];
+    char value[255];
 } token;
 
 token next_token();
 void tokenize(FILE *fp);
+
+#endif

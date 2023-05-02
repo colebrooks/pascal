760248f5181a31fa9cee6decbde8b42dae5dfed7
colebrooks <colespencerbrooks@gmail.com>
2023-05-02 12:10:30 -0600

    Implement dynamic memory read of file

--- src/main.c
+++ src/main.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "tokenizer.h"
+
+char *source = NULL;
+
+void read_file(char *file) {
+    FILE *fp = fopen(file, "r");
+    if(fp != NULL) {
+        if (fseek(fp, 0L, SEEK_END) == 0) {
+            long bufsize = ftell(fp);
+            if (bufsize == -1) { /* Error */ }
+
+            source = malloc(sizeof(char) * (bufsize + 1));
+            rewind(fp);
+
+            size_t newLen = fread(source, sizeof(char), bufsize, fp);   
+            if (ferror(fp) != 0) { /* Error reading file */ }
+            else {
+                source[newLen++] = '\0'; 
+            }
+        }
+    }
+    fclose(fp);
+}
 
 int main(int argc, char *argv[]) {
     if(argc != 2) {
         printf("Please input a file to parse.\n");
         return 1;
     } else {
-        FILE *fp;
-        fp = fopen(argv[1], "r");
-        tokenize(fp);
+        read_file(argv[1]);
     }
-
+    tokenize(source);
+    free(source);
     return 0;
 }

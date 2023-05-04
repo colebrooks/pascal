82f8cd4f1e5385253697c95dda9dc237c3e3003a
colebrooks <colespencerbrooks@gmail.com>
2023-05-04 15:57:58 -0600

    Add future function for dynamic read of file

--- src/main.c
+++ src/main.c
@@ -3,36 +3,37 @@
 
 #include "tokenizer.h"
 
-char *source = NULL;
+// char *source = NULL;
 
-void read_file(char *file) {
-    FILE *fp = fopen(file, "r");
-    if(fp != NULL) {
-        if (fseek(fp, 0L, SEEK_END) == 0) {
-            long bufsize = ftell(fp);
-            if (bufsize == -1) { /* Error */ }
-
-            source = malloc(sizeof(char) * (bufsize + 1));
-            rewind(fp);
-
-            size_t newLen = fread(source, sizeof(char), bufsize, fp);   
-            if (ferror(fp) != 0) { /* Error reading file */ }
-            else {
-                source[newLen++] = '\0'; 
-            }
-        }
-    }
-    fclose(fp);
-}
+// Potential fucture implementation of eager file reading
+// void read_file(char *file) {
+//     FILE *fp = fopen(file, "r");
+//     if(fp != NULL) {
+//         if (fseek(fp, 0L, SEEK_END) == 0) {
+//             long bufsize = ftell(fp);
+//             if (bufsize == -1) { /* Error */ }
+// 
+//             source = malloc(sizeof(char) * (bufsize + 1));
+//             rewind(fp);
+// 
+//             size_t newLen = fread(source, sizeof(char), bufsize, fp);   
+//             if (ferror(fp) != 0) { /* Error reading file */ }
+//             else {
+//                 source[newLen++] = '\0'; 
+//             }
+//         }
+//     }
+//     fclose(fp);
+// }
 
 int main(int argc, char *argv[]) {
+    FILE *source;
     if(argc != 2) {
         printf("Please input a file to parse.\n");
         return 1;
     } else {
-        read_file(argv[1]);
+        source = fopen(argv[1], "r");
     }
     tokenize(source);
-    free(source);
     return 0;
 }

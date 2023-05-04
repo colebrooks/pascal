9c89008ec83696632e01eb2904b0320b31c0608f
colebrooks <colespencerbrooks@gmail.com>
2023-05-04 15:58:48 -0600

    Implement separator/operator matching

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,9 +1,12 @@
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 #include "tokenizer.h"
 
-const char *keywords[] = {"function", "integer", "longint", "begin", "if", "then", "else", "end", "var", "for", "to", "do", "writeln"};
+const char *keywords[] = {"program", "function", "integer", "longint", "begin", "if", "then", "else", "end", "var", "for", "to", "do", "writeln"};
+const char *separators[] = {";", "(", ")", ",", "'"};
+const char *operators[] = {"+", "-" , "*" , "/", "%", "=", "<>", "<", ">", "<=", ">=", ":="};
 token tokens[255];
 int num_tokens = 0;
 
@@ -17,7 +20,7 @@ void print_token(token tok) {
             strcpy(type, "KEYWORD");
             break;
         case 2:
-            strcpy(type, "SEPARTATOR");
+            strcpy(type, "SEPARATOR");
             break;
         case 3:
             strcpy(type, "OPERATOR");
@@ -39,46 +42,109 @@ char peek(FILE *fp) {
     return c;
 }
 
-void read_word(FILE *fp, char *word) {
+void read_word(FILE *fp, char *val) {
     char buff[255] = "";
-
     char c;
     int len = 0;
+
     while(isalpha((c = fgetc(fp)))) {
         buff[len++] = c;
     }
-    strcpy(word, buff);
+    ungetc(c, fp);  // Unget final nonalpha char
+    strcpy(val, buff);
 }
 
-token match_word(char word[]) {
+token match_word(char val[]) {
     token tok;
-    strcpy(tok.value, word);
+    strcpy(tok.value, val);
 
     int len = sizeof(keywords) / sizeof(keywords[0]);
+    int keyword = 0;
     for(int i = 0; i < len; i++) {
-        if(!strcmp(word, keywords[i])) {
-            tok.type = KEYWORD;
-            print_token(tok);
-            return tok;
-        }
+        if(!strcmp(val, keywords[i])) {
+            keyword = 1;
+            break;
+        }    
     }
+    tok.type = keyword ? KEYWORD : IDENTIFIER;
+    return tok;
 }
 
-void read_tokens(FILE *fp) {
-    int current_token = 0;
+void read_num(FILE *fp, char *val) {
+    char buff[255] = "";
     char c;
-    while (!feof(fp)) {
-        char c = peek(fp);
-        if(isalpha(c)) {
-            char word[255];
-            read_word(fp, word);
-            tokens[current_token++] = match_word(word);
-        } else {
-            fgetc(fp);
+    int len = 0;
+
+    while(isdigit(c = fgetc(fp))) {
+        buff[len++] = c;
+    }
+    ungetc(c, fp);  // Unget final nondigit char
+    strcpy(val, buff);
+}
+
+token match_num(char *val) {
+    token tok;
+    strcpy(tok.value, val);
+    tok.type = LITERAL;
+    return tok;
+}
+
+void read_sym(FILE *fp, char *val) {
+    char buff[255] = "";
+    char c;
+    int len = 0;
+    c = fgetc(fp);
+    buff[len++] = c;
+    if(c == '<' || c == '>' || c == '*' || c == ':' || c == '+' || c == '-' || c == '/' || c == '(' || c == '.') {
+        c = fgetc(fp);
+        if(c != ' ' && c != '\t' && c != '\n' && !isalpha(c) && !isdigit(c)) {
+            buff[len++] = c;
         }
     }
+    strcpy(val, buff);
+}
+
+token match_sym(char *val) {
+    token tok;
+    strcpy(tok.value, val);
+    int operator = 0;
+
+    int len = sizeof(operators) / sizeof(operators[0]);
+    for(int i = 0; i < len; i++) {
+        if(!strcmp(val, operators[i])) {
+            operator = 1;
+        }
+    }
+    tok.type = operator ? OPERATOR : SEPARATOR;
+    return tok;
+}
+
+token match_token(FILE *fp) {
+    char c = peek(fp); 
+    while(c == ' ' || c == '\t' || c == '\n') {
+        fgetc(fp);
+        c = peek(fp);
+    }
+    
+    char val[255] = "";
+    token ret;
+    if(isalpha(c)) {
+        read_word(fp, val);
+        ret = match_word(val);
+    } else if(isdigit(c)) {
+        read_num(fp, val);
+        ret = match_num(val);
+    } else {
+        read_sym(fp, val);
+        ret = match_sym(val);
+    }
+    print_token(ret);
+    return ret;
 }
 
 void tokenize(FILE *fp) {
-    read_tokens(fp);
+    int current_token = 0;
+    while (!feof(fp)) {
+        tokens[current_token++] = match_token(fp);
+    }
 } 

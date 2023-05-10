b493254fc87377e1cc231bc0a6dfbbe38286a595
colebrooks <colespencerbrooks@gmail.com>
2023-05-10 11:35:35 -0600

    Remove eager file read into string

--- src/main.c
+++ src/main.c
@@ -3,29 +3,6 @@
 
 #include "tokenizer.h"
 
-// char *source = NULL;
-
-// Potential fucture implementation of eager file reading
-// void read_file(char *file) {
-//     FILE *fp = fopen(file, "r");
-//     if(fp != NULL) {
-//         if (fseek(fp, 0L, SEEK_END) == 0) {
-//             long bufsize = ftell(fp);
-//             if (bufsize == -1) { /* Error */ }
-// 
-//             source = malloc(sizeof(char) * (bufsize + 1));
-//             rewind(fp);
-// 
-//             size_t newLen = fread(source, sizeof(char), bufsize, fp);   
-//             if (ferror(fp) != 0) { /* Error reading file */ }
-//             else {
-//                 source[newLen++] = '\0'; 
-//             }
-//         }
-//     }
-//     fclose(fp);
-// }
-
 int main(int argc, char *argv[]) {
     if(argc != 2) {
         printf("Please input a file to parse.\n");

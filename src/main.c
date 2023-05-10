0be9369ef54c32316d799644a74f44f9a26bd7e7
colebrooks <colespencerbrooks@gmail.com>
2023-05-10 14:56:14 -0600

    Add parser

--- src/main.c
+++ src/main.c
@@ -2,17 +2,15 @@
 #include <stdlib.h>
 
 #include "tokenizer.h"
+#include "parser.h"
 
 int main(int argc, char *argv[]) {
     if(argc != 2) {
         printf("Please input a file to parse.\n");
         return 1;
     } else {
-        token *tok = tokenize(argv[1]);
-        while(tok != NULL) {
-            print_token(*tok);
-            tok = tok->next;
-        }
+        token *head = tokenize(argv[1]);
+        parse(head);
     }
     return 0;
 }
